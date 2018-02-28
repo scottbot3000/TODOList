a60a5abbfe5db5f328e6310054a366742a014006
scottbot3000 <scott.carbonbased@gmail.com>
2018-02-27 19:41:17 -0800

    Initial Commit

---
+++ TODOList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TODOList
+//
+//  Created by Scott Penn on 2/27/18.
+//  Copyright © 2018 Scott Bot Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
